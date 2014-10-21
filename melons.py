1991227bd6bc98ff817e9bf6a8a006f151c6f126
Hackbright Student <info@hackbrightacademy.com>
2014-10-20 17:55:48 -0700

    Added cart links back to melon detail pages

--- melons.py
+++ melons.py
@@ -10,38 +10,42 @@ app.jinja_env.undefined = jinja2.StrictUndefined
 @app.route("/")
 def index():
     """This is the 'cover' page of the ubermelon site"""
+
     return render_template("index.html")
 
 @app.route("/melons")
 def list_melons():
     """This is the big page showing all the melons ubermelon has to offer"""
+
     melons = model.get_melons()
-    return render_template("all_melons.html",
-                           melon_list = melons)
+    return render_template("all_melons.html", melon_list = melons)
 
 @app.route("/melon/<int:id>")
 def show_melon(id):
     """This page shows the details of a given melon, as well as giving an
     option to buy the melon."""
+
     melon = model.get_melon_by_id(id)
 
-    return render_template("melon_details.html",
-                  display_melon = melon)
+    return render_template("melon_details.html", display_melon = melon)
 
 @app.route("/cart")
 def shopping_cart():
     """TODO: Display the contents of the shopping cart. The shopping cart is a
     list held in the session that contains all the melons to be added. Check
     accompanying screenshots for details."""
+
     melon_dict = {}
     cart_list = session.get("cart", [])
+
     if not cart_list:
         flash("Your cart is empty")
+
     # create a dictionary where key = melon_id and value = dictionary with key=quantity, key=total
     for melon_id in cart_list:
         melon_dict.setdefault(melon_id, {})
         melon_dict[melon_id]["quantity"] = melon_dict[melon_id].get("quantity", 0) + 1
-    melon_info_list = [model.get_melon_by_id(melon_id) for melon_id in melon_dict.keys()] 
+    melon_info_list = [model.get_melon_by_id(melon_id) for melon_id in melon_dict] 
 
     cart_total = 0 
     for melon in melon_info_list:
@@ -53,6 +57,7 @@ def shopping_cart():
         melon_dict[melon.id]["formatted_total_price"] = "$%.2f" % total
 
     cart_total_string = "Total: $%.2f" % cart_total
+
     return render_template("cart.html", melon_list = melon_info_list, quantotals = melon_dict, 
         cart_total = cart_total_string)
     
@@ -65,9 +70,11 @@ def add_to_cart():
     Intended behavior: when a melon is added to a cart, redirect them to the
     shopping cart page, while displaying the message
     "Successfully added to cart" """
+
     id = int(request.form.get("id"))
     session.setdefault("cart", []).append(id)
     flash("Melon successfully added to cart")
+
     return redirect("/cart")
 
 @app.route("/login", methods=["GET"])
@@ -79,9 +86,11 @@ def show_login():
 def process_login():
     """TODO: Receive the user's login credentials located in the 'request.form'
     dictionary, look up the user, and store them in the session."""
+
     user_email = request.form.get("email")
     user_password = request.form.get("password")
     user_info = model.get_customer_by_email(user_email)
+
     if user_info:
         session["user"] = user_info
         flash("Login Successful")
@@ -92,6 +101,7 @@ def process_login():
 def process_logout():
     session["user"] = []
     flash("Logout successful")
+
     return redirect("/melons")
 
 
@@ -99,7 +109,9 @@ def process_logout():
 def checkout():
     """TODO: Implement a payment system. For now, just return them to the main
     melon listing page."""
+
     flash("Sorry! Checkout will be implemented in a future version of ubermelon.")
+    
     return redirect("/melons")
 
 if __name__ == "__main__":
